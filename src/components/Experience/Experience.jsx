a415f90e34757c9d7c2f0bbc9656f54f714d13d7
rferna214 <rferna214@gmail.com>
2024-02-29 21:28:11 -0500

    added images to the skills folder

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,25 @@
 import React from 'react' 
-
+import skills from "../../data/skills.json"
+import { getImageUrl } from '../../utils'
 export const Experience = () => {
     return(
-       <div>Experience</div>
+       <section id="experience">
+        <h2>
+            Experience
+        </h2>
+        <div>
+            <div>{
+                skills.map((skills, id) => {
+                    return <div key={id}>
+                        <div><img src={getImageUrl(skills.imageSrc)} alt={skills.title} /></div>
+                    </div>
+                }
+                )
+                }
+            </div>
+            <ul></ul>
+        </div>
+
+       </section>
     )
 }
