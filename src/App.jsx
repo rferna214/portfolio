ecb101db2d91f9c96549c4b11bca5d9ef0a5dfff
rferna214 <rferna214@gmail.com>
2024-03-05 00:17:55 -0500

    Created Project card components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import styles from "./App.module.css";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { About } from "./components/About/About";
-
+import { Projects } from "./components/Projects/Projects";
 import { Experience } from "./components/Experience/Experience";
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
       <Hero/>
       <About/>
       <Experience/>
+      <Projects/>
     </div>
   )
 }
