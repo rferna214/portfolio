561134263ff778143c85536cbe93ab324195cc78
rferna214 <rferna214@gmail.com>
2024-03-08 18:39:29 -0500

    Project card readable now

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -10,9 +10,10 @@ export const ProjectCard = ({ project: {title, imageSrc, description, skills, de
             <h3 className={styles.title}>{title}</h3>
             <p className={styles.description}>{description}</p>
             <ul className={styles.skills}>{skills.map((skill, id) => {
+                return(
                 <li className={styles.skill} key={id}>
                     {skill}
-                </li>
+                </li>);
             })}
             </ul>
             <div className={styles.links}>
