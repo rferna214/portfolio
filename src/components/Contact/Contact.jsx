a1b8944a4bef206ee384f319f1322c7d2497ffb5
rferna214 <rferna214@gmail.com>
2024-03-10 14:00:03 -0400

    Finished most of proejct just need to fix project section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,23 +4,23 @@ import styles from "./Contact.module.css";
 
 export const Contact = () => {
   return (
-    <footer>
-        <div>
+    <footer id='contact' className={styles.container}>
+        <div className={styles.text}>
             <h2>Contact</h2>
             <p>
                 Feel free to reach out!
             </p>
         </div>
-        <ul>
-            <li>
+        <ul className={styles.links}>
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
                 <a href="mailto:Rferna214@gmail.com">Rferna214@gmail.com</a>
             </li>
-            <li>
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn Icon" />
                 <a href="https://www.linkedin.com/in/rferna214">linkedin.com/Rferna214</a>
             </li>
-            <li>
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/githubIcon.png")} alt="GitHub Icon" />
                 <a href="https://www.github.com/rferna214">github.com/Rferna214</a>
             </li>
