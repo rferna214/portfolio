bf24407f45f3f5fe06157f6c9a749f6766dd5e98
rferna214 <rferna214@gmail.com>
2024-03-11 13:19:53 -0400

    bug fixes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,12 +8,11 @@ export const Hero = () => {
     return (
        <section className={styles.container}>
         <div className={styles.content}>
-            <h1 className={styles.title}>
-                Hey, I'm Ronald</h1>
+            <h1 className={styles.title}>Hey, I'm Ronald</h1>
                 <p className={styles.description}>I'm a Software Developer with experience in mobile applications and web applications. Reach out if you'd like to learn more!</p>
-                <a href ="mailto:rferna214@gmail.com" className={styles.contactBtn}> Contact Me</a>
+                <a href ="mailto:rferna214@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
-        <img src={getImageUrl("hero/image1.png")} alt=" Image of me" className={styles.heroImg}/>
+        <img src={getImageUrl("hero/image1.png")} alt="Image of me" className={styles.heroImg}/>
         <div className={styles.topBlur}/>
         <div className={styles.bottomBlur}/>
        </section>
